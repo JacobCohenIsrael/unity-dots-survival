f4815b7e0d57f5febb57fc3b4774548d3322d246
Jacob Cohen <jacobcohenisrael@gmail.com>
2025-04-27 09:30:15 +0300

    Converted movement to job and added stop distance

--- Assets/Scripts/Systems/UnitMovementSystem.cs
+++ Assets/Scripts/Systems/UnitMovementSystem.cs
@@ -10,19 +10,44 @@ partial struct UnitMovementSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (localTransform, unitMovement, physicsVelocity) 
-                 in SystemAPI.Query<
-                     RefRW<LocalTransform>, 
-                     RefRO<UnitMovement>, 
-                     RefRW<PhysicsVelocity>>())
-        {
-            var moveDirection = unitMovement.ValueRO.TargetPosition - localTransform.ValueRO.Position;
-            moveDirection = math.lengthsq(moveDirection) > 0f ? math.normalize(moveDirection) : float3.zero;
+        var job = new UnitMovementJob { deltaTime = SystemAPI.Time.DeltaTime };
+        job.ScheduleParallel();        
+        // foreach (var (localTransform, unitMovement, physicsVelocity) 
+        //          in SystemAPI.Query<
+        //              RefRW<LocalTransform>, 
+        //              RefRO<UnitMovement>, 
+        //              RefRW<PhysicsVelocity>>())
+        // {
+        //     var moveDirection = unitMovement.ValueRO.TargetPosition - localTransform.ValueRO.Position;
+        //     moveDirection = math.lengthsq(moveDirection) > 0f ? math.normalize(moveDirection) : float3.zero;
+        //
+        //     localTransform.ValueRW.Rotation = 
+        //         math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotationSafe(moveDirection, math.up()), SystemAPI.Time.DeltaTime * unitMovement.ValueRO.RotationSpeed);
+        //     physicsVelocity.ValueRW.Linear = moveDirection * unitMovement.ValueRO.MoveSpeed;
+        //     physicsVelocity.ValueRW.Angular = float3.zero;
+        // }
+    }
+}
 
-            localTransform.ValueRW.Rotation = 
-                math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotationSafe(moveDirection, math.up()), SystemAPI.Time.DeltaTime * unitMovement.ValueRO.RotationSpeed);
-            physicsVelocity.ValueRW.Linear = moveDirection * unitMovement.ValueRO.MoveSpeed;
-            physicsVelocity.ValueRW.Angular = float3.zero;
+[BurstCompile]
+public partial struct UnitMovementJob : IJobEntity
+{
+    public float deltaTime;
+    public void Execute(ref LocalTransform localTransform, in UnitMovement unitMovement, ref PhysicsVelocity physicsVelocity)
+    {
+        var moveDirection = unitMovement.TargetPosition - localTransform.Position;
+
+        if (math.lengthsq(moveDirection) < 2f)
+        {
+            physicsVelocity.Linear = float3.zero;
+            physicsVelocity.Angular = float3.zero;
+            return;       
         }
+        
+        moveDirection = math.lengthsq(moveDirection) > 0f ? math.normalize(moveDirection) : float3.zero;
+        localTransform.Rotation = 
+            math.slerp(localTransform.Rotation, quaternion.LookRotationSafe(moveDirection, math.up()), deltaTime * unitMovement.RotationSpeed);
+        physicsVelocity.Linear = moveDirection * unitMovement.MoveSpeed;
+        physicsVelocity.Angular = float3.zero;
     }
 }
